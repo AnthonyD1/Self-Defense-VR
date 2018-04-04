6c65c65e36f9002b1784d30edd47c82c15616f55
AnthonyD1 <anthonydelgado@hotmail.com>
2018-04-04 15:00:31 -0500

    Added Jab pad animations to Boris
    Added scripts to change hands for the jab pad positions. Will get other pad positions soon.

---
+++ SelfDefenseVR/Assets/Scripts/ChangeToLeft.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeToLeft : MonoBehaviour {
+
+    public GameObject LeftPath;
+    public GameObject RightPath;
+    private Animator BorisAnimator;
+
+
+    private void Awake()
+    {
+        BorisAnimator = GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("leftHand"))
+        {
+
+            BorisAnimator.SetBool("HitLeftPad", true);
+
+            //bLeftElbow.transform.Rotate(new Vector3(0, 70, 0));//t-pose
+
+            //bRightElbow.transform.Rotate(new Vector3 (0, 70, 0));//t-pose
+
+            LeftPath.SetActive(false);
+            RightPath.SetActive(true);
+
+            BorisAnimator.SetBool("HitRightPad", false);
+        }
+    }
+}
