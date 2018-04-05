4fd8da347bb8cb67e6e872a7dbd2314fc645c346
jjproby <jjproby719@gmail.com>
2018-04-05 13:34:18 -0500

    Made all animations for Boris
    All in a new scene. Animations are made for Boris all with their own animation controller. Also there is a different script for the hooks. It needed to happen.

---
+++ SelfDefenseVR/Assets/Scripts/ChangeToLeftHook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeToLeftHook : MonoBehaviour {
+
+    public GameObject LeftPath;
+    public GameObject RightPath;
+    private Animator BorisAnimator;
+
+
+    private void Awake()
+    {
+        BorisAnimator = GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("rightHand"))
+        {
+
+            BorisAnimator.SetBool("HitLeftPad", true);
+
+            LeftPath.SetActive(false);
+            RightPath.SetActive(true);
+
+            BorisAnimator.SetBool("HitRightPad", false);
+        }
+    }
+}
---
+++ SelfDefenseVR/Assets/Scripts/ChangeToRightHook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeToRightHook : MonoBehaviour {
+
+    public GameObject LeftPath;
+    public GameObject RightPath;
+    private Animator BorisAnimator;
+
+
+    private void Awake()
+    {
+        BorisAnimator = GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("leftHand"))
+        {
+            BorisAnimator.SetBool("HitRightPad", true);
+
+            LeftPath.SetActive(true);
+            RightPath.SetActive(false);
+
+            BorisAnimator.SetBool("HitLeftPad", false);
+        }
+    }
+}
