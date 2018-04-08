4eb9d727ccee6bae3b5e6c502be3d7a19f7eb913
STOzaki <SOzaki19@cornellcollege.edu>
2018-04-08 15:31:50 -0500

    Fixed Scene Change
    Added a new SceneChange script and then deleted the other scene changes, and fixed all of the control panels in every scene.

---
+++ SelfDefenseVR/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    private string sceneName;
+    private void OnCollisionEnter(Collision other)
+    {
+        // grabs the name of the parent and use that name to change scenes
+        sceneName = this.transform.parent.name;
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
