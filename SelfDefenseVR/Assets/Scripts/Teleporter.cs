0d63d16a52f3d3900463dea0ddf13a883377e208
AnthonyD1 <>
2018-04-08 15:10:00 -0500

    Implemented Teleportation
    Press down the "A" button and look where you want to teleport then pull the trigger.
    -Note: currently there a bug where if you teleport in front of Boris, he pushes you back.

---
+++ SelfDefenseVR/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+    public GameObject TeleportMarker;
+    public Transform Player;
+    public float RayLength = 50f;
+    public OVRInput.Controller Controller;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, RayLength)) {
+            if(hit.collider.tag == "Ground" && OVRInput.Get(OVRInput.Button.One, Controller)) {
+                if (!TeleportMarker.activeSelf) {
+                    TeleportMarker.SetActive(true);
+                }
+                TeleportMarker.transform.position = hit.point;
+
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, Controller)) {
+                     Player.position = new Vector3(hit.point.x, Player.position.y, hit.point.z);
+                }
+            } else {
+                TeleportMarker.SetActive(false);
+            }
+        } else {
+            TeleportMarker.SetActive(false);
+        }
+
+	}
+
+
+}
