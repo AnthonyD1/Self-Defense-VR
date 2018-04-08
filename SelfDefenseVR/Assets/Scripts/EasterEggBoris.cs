eeac9f438a3c4ca0bbdad56ff0d75b18982ea5fb
Jjproby <jjproby719@gmail.com>
2018-04-08 17:14:03 -0500

    Boris Easter Egg
    Boris now flys if you punch him to many times

---
+++ SelfDefenseVR/Assets/Scripts/EasterEggBoris.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggBoris : MonoBehaviour {
+
+    private int hits = 0;
+    public GameObject Boris;
+    private GameObject BorisHead;
+
+    private void OnTriggerExit(Collider other)
+    {
+        hits = hits + 1;
+        if (hits >= 10)
+        {
+            Rigidbody BorisBody = Boris.AddComponent<Rigidbody>();
+            BorisBody.useGravity = false;
+            BoxCollider BorisHeadBox = BorisHead.GetComponent<BoxCollider>();
+            BorisHeadBox.isTrigger = false;
+        }
+    }
+}
