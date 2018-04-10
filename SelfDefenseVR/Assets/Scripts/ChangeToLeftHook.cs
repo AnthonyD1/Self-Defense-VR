3ae51b7901a1b0c638e22fa27d6511b13e15f26c
jjproby <jjproby719@gmail.com>
2018-04-10 15:18:45 -0500

    Comments on ChangeLeftHook

--- SelfDefenseVR/Assets/Scripts/ChangeToLeftHook.cs
+++ SelfDefenseVR/Assets/Scripts/ChangeToLeftHook.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ChangeToLeftHook : MonoBehaviour {
 
+    //the paths for the punches
     public GameObject LeftPath;
     public GameObject RightPath;
+    
+    //Boris's body
     public GameObject Boris;
+    
+    //The animations for Boris
     private Animator BorisAnimator;
+    
+    //The sound for punching
     private AudioSource punchSound;
+   
+    //audio clip for controller vibration
     public AudioClip HapticFeedback;
+    
+    //audio clip for Boris's voice clips
     public AudioClip BorisVoice;
+    
+    //make sure that Boris's voice doesn't overlap
     private bool hasPlayed = false;
-
 
     private void Awake()
     {
@@ -20,6 +32,7 @@ public class ChangeToLeftHook : MonoBehaviour {
         punchSound = GetComponent<AudioSource>();
     }
 
+    //plays punch sound when player hits the pad
     private void OnTriggerEnter(Collider other)
     {
         if (hasPlayed == false) {
@@ -27,7 +40,9 @@ public class ChangeToLeftHook : MonoBehaviour {
             hasPlayed = true;
         }
 
+        //contoller vibrates when pad is hit
         OVRHapticsClip hapticsClip = new OVRHapticsClip(HapticFeedback);
+        //checks when hand hit the pad and makes that hand vibrate
         if (other.gameObject.CompareTag("rightHand")) {
             OVRHaptics.RightChannel.Preempt(hapticsClip);
         }
@@ -35,9 +50,11 @@ public class ChangeToLeftHook : MonoBehaviour {
 
     private void OnTriggerExit(Collider other)
     {
+        //makes sure that the use used the right hand to punch the pad
         if (other.gameObject.CompareTag("rightHand"))
         {
 
+            //switches hands and path. Plays Boris's next voice line
             BorisAnimator.SetBool("HitLeftPad", true);
 
             LeftPath.SetActive(false);
