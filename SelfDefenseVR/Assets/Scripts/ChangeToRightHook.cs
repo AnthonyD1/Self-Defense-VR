d5de5e41ab0ffb73fec3dbd05f45c5d9529722ad
jjproby <jjproby719@gmail.com>
2018-04-10 20:33:07 -0500

    Commented on ChangeToRightHooks

--- SelfDefenseVR/Assets/Scripts/ChangeToRightHook.cs
+++ SelfDefenseVR/Assets/Scripts/ChangeToRightHook.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class ChangeToRightHook : MonoBehaviour {
 
+    //the paths for the punches
     public GameObject LeftPath;
     public GameObject RightPath;
+    
+    //Boris's body
     public GameObject Boris;
+    
+    //The animations for Boris
     private Animator BorisAnimator;
+    
+    //The sound for punching
     private AudioSource punchSound;
+    
+    //Audio clip for controller vibration
     public AudioClip HapticFeedback;
+    
+    //audio clip for Boris's voice clips
     public AudioClip BorisVoice;
+    
+    //makes sure that Boris's voice doesn't overlap
     private bool hasPlayed = false;
 
 
@@ -20,14 +33,17 @@ public class ChangeToRightHook : MonoBehaviour {
         punchSound = GetComponent<AudioSource>();
     }
 
+    //plays punch sound when player hits pad
     private void OnTriggerEnter(Collider other)
     {
         if (hasPlayed == false) {
             punchSound.Play();
             hasPlayed = true;
         }
-
+    
+        //controller vibrates when pad is hit
         OVRHapticsClip hapticsClip = new OVRHapticsClip(HapticFeedback);
+        //decides what hand to vibrate on
         if (other.gameObject.CompareTag("leftHand")) {
             OVRHaptics.LeftChannel.Preempt(hapticsClip);
         }
@@ -35,8 +51,10 @@ public class ChangeToRightHook : MonoBehaviour {
 
     private void OnTriggerExit(Collider other)
     {
+        //makes sure the user used the left hand to hit the pad
         if (other.gameObject.CompareTag("leftHand"))
         {
+            //switches hands and path. Plays Boris's next voice line
             BorisAnimator.SetBool("HitRightPad", true);
 
             LeftPath.SetActive(true);
