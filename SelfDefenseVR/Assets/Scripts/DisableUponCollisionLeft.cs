38d625fecd046cb7c7f130fefce8df111881faa9
Anthony Delgado <anthonydelgado@hotmail.com>
2018-04-11 03:00:56 -0500

    added documentation

--- SelfDefenseVR/Assets/Scripts/DisableUponCollisionLeft.cs
+++ SelfDefenseVR/Assets/Scripts/DisableUponCollisionLeft.cs
@@ -2,23 +2,33 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
+/**
+* Attached to each sphere that is on a left path punch, this script will
+* disable the current sphere's collider and enable the next's collider.
+*/
 public class DisableUponCollisionLeft : MonoBehaviour
 {
+    // get this sphere's collider
     private SphereCollider sphereCollider;
 
-	// Use this for initialization
+	/**
+	* Initialize the sphere's collider.
+	*/
 	void Start ()
     {
         sphereCollider = this.GetComponent<SphereCollider>();
 	}
 
+    /**
+    * Returns the Transform of the next sphere in the path.
+    */
     private Transform GetNext()
     {
-        Transform currentTransform = transform; // me
-        Transform parent = transform.parent; // daddy
+        Transform currentTransform = transform; // current sphere's transform
+        Transform parent = transform.parent; // parent object's transform
         var childcount = parent.childCount;
         
-        // loop through children until find me, then get next index
+        // loop through children until find current sphere is found, then get index of next one
         for (int i = 0; i < childcount - 1; i++)
         {
             if (currentTransform == parent.GetChild(i))
@@ -29,6 +39,10 @@ public class DisableUponCollisionLeft : MonoBehaviour
         return null;
     }
 
+    /**
+    * Disables the sphere that is collided by the left hand, and enables the next sphere
+    * along with it's collider.
+    */
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("leftHand"))
